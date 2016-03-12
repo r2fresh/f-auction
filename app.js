68bc24bc5440a55f15640d8ac618d1947fa64847
r2fresh <r2fresh78@gmail.com>
2016-03-13 01:21:04 +0900

    index.html add

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!!!!');
-});
+app.get('/',function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!');
