554dac6d513b12cbaccc2d997a1719d75d332718
r2fresh <r2fresh78@gmail.com>
2016-03-13 03:45:32 +0900

    최저경쟁가격 개발중

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static('public'))
+
 app.get('/',function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/preAuction',function(req, res){
+    res.sendFile(path.join(__dirname + '/preAuction.html'))
 })
 
 app.listen(app.get('port'), function () {
