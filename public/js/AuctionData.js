85c0c8e0ef9b89b3b0525997bf88fa927a4c29cc
r2fresh <r2fresh78@gmail.com>
2016-03-30 17:37:06 +0900

    최소가격입찰 테스트

--- public/js/AuctionData.js
+++ public/js/AuctionData.js
@@ -5,95 +5,26 @@ define({
         {'name':'LG','state':false}
     ],
     'bidders': [
-        {'name':'KT', 'price':0, 'vs':'win'},
-        {'name':'SK', 'price':0, 'vs':'win'},
-        {'name':'LG', 'price':0, 'vs':'win'}
+        {'name':'KT', 'price':'', 'vs':'win'},
+        {'name':'SK', 'price':'', 'vs':'win'},
+        {'name':'LG', 'price':'', 'vs':'win'}
     ],
+
     'defaultPriceList':[
-        {'name':'priceA','bandWidth':40,'price':null,'type':'wideBand'},
-        {'name':'priceB','bandWidth':20,'price':null,'type':'narrow'},
-        {'name':'priceC','bandWidth':20,'price':null,'type':'wideBand'},
-        {'name':'priceD','bandWidth':40,'price':null,'type':'wideBand'},
-        {'name':'priceE','bandWidth':20,'price':null,'type':'narrow'}
+        {'name':'priceA','bandWidth':40,'price':'','type':'wideBand'},
+        {'name':'priceB','bandWidth':20,'price':'','type':'narrow'},
+        {'name':'priceC','bandWidth':20,'price':'','type':'wideBand'},
+        {'name':'priceD','bandWidth':40,'price':'','type':'wideBand'},
+        {'name':'priceE','bandWidth':20,'price':'','type':'narrow'}
     ],
 
     'startPriceList':[
-        {'name':'priceA','bandWidth':40,'price':7620,'type':'wideBand'},
-        {'name':'priceB','bandWidth':20,'price':4513,'type':'narrow'},
-        {'name':'priceC','bandWidth':20,'price':3816,'type':'wideBand'},
-        {'name':'priceD','bandWidth':40,'price':6553,'type':'wideBand'},
-        {'name':'priceE','bandWidth':20,'price':3277,'type':'narrow'}
+        {'name':'priceA','bandWidth':40,'price':'7620','type':'wideBand'},
+        {'name':'priceB','bandWidth':20,'price':'4513','type':'narrow'},
+        {'name':'priceC','bandWidth':20,'price':'3816','type':'wideBand'},
+        {'name':'priceD','bandWidth':40,'price':'6553','type':'wideBand'},
+        {'name':'priceE','bandWidth':20,'price':'3277','type':'narrow'}
     ],
-
-    'startPrices': {
-        'frequency' : [
-            {
-                'name': 'A',
-                'bandWidth':40,
-                'hertz':'700',
-                'type':'wideBand',
-                'winBidder': '',
-                'winPrice': 1200,
-                'bidders': [
-                    {'name':'KT', 'price':1200, 'vs':'win'},
-                    {'name':'SK', 'price':1200, 'vs':'win'},
-                    {'name':'LG', 'price':1200, 'vs':'win'}
-                ]
-            },
-            {
-                'name': 'B',
-                'bandWidth':20,
-                'hertz':'18',
-                'type':'narrow',
-                'winBidder': '',
-                'winPrice': 2300,
-                'bidders': [
-                    {'name':'KT', 'price':2300, 'vs':'win'},
-                    {'name':'SK', 'price':2300, 'vs':'win'},
-                    {'name':'LG', 'price':2300, 'vs':'win'}
-                ]
-            },
-            {
-                'name': 'C',
-                'bandWidth':20,
-                'hertz':'21',
-                'type':'wideBand',
-                'winBidder': '',
-                'winPrice': 3150,
-                'bidders': [
-                    {'name':'KT', 'price':3150, 'vs':'win'},
-                    {'name':'SK', 'price':3150, 'vs':'win'},
-                    {'name':'LG', 'price':3150, 'vs':'win'}
-                ]
-            },
-            {
-                'name': 'D',
-                'bandWidth':40,
-                'hertz':'26',
-                'type':'wideBand',
-                'winBidder': '',
-                'winPrice': 3816,
-                'bidders': [
-                    {'name':'KT', 'price':3816, 'vs':'win'},
-                    {'name':'SK', 'price':3816, 'vs':'win'},
-                    {'name':'LG', 'price':3816, 'vs':'win'}
-                ]
-            },
-            {
-                'name': 'E',
-                'bandWidth':20,
-                'hertz':'26',
-                'type':'narrow',
-                'winBidder': '',
-                'winPrice': 3277,
-                'bidders': [
-                    {'name':'KT', 'price':3277, 'vs':'win'},
-                    {'name':'SK', 'price':3277, 'vs':'win'},
-                    {'name':'LG', 'price':3277, 'vs':'win'}
-                ]
-            },
-        ]
-    },
     'round': {
         'name':'',
         'frequency' : [
@@ -103,11 +34,11 @@ define({
                 'hertz':'700',
                 'type':'wideBand',
                 'winBidder': '',
-                'winPrice': 0,
+                'winPrice': '',
                 'bidders': [
-                    {'name':'KT', 'price':0},
-                    {'name':'SK', 'price':0},
-                    {'name':'LG', 'price':0}
+                    {'name':'KT', 'price':''},
+                    {'name':'SK', 'price':''},
+                    {'name':'LG', 'price':''}
                 ]
             },
             {
@@ -116,11 +47,11 @@ define({
                 'hertz':'18',
                 'type':'narrow',
                 'winBidder': '',
-                'winPrice': 0,
+                'winPrice': '',
                 'bidders': [
-                    {'name':'KT', 'price':0},
-                    {'name':'SK', 'price':0},
-                    {'name':'LG', 'price':0}
+                    {'name':'KT', 'price':''},
+                    {'name':'SK', 'price':''},
+                    {'name':'LG', 'price':''}
                 ]
             },
             {
@@ -129,11 +60,11 @@ define({
                 'hertz':'21',
                 'type':'wideBand',
                 'winBidder': '',
-                'winPrice': 0,
+                'winPrice': '',
                 'bidders': [
-                    {'name':'KT', 'price':0},
-                    {'name':'SK', 'price':0},
-                    {'name':'LG', 'price':0}
+                    {'name':'KT', 'price':''},
+                    {'name':'SK', 'price':''},
+                    {'name':'LG', 'price':''}
                 ]
             },
             {
@@ -142,11 +73,11 @@ define({
                 'hertz':'26',
                 'type':'wideBand',
                 'winBidder': '',
-                'winPrice': 0,
+                'winPrice': '',
                 'bidders': [
-                    {'name':'KT', 'price':0},
-                    {'name':'SK', 'price':0},
-                    {'name':'LG', 'price':0}
+                    {'name':'KT', 'price':''},
+                    {'name':'SK', 'price':''},
+                    {'name':'LG', 'price':''}
                 ]
             },
             {
@@ -155,15 +86,85 @@ define({
                 'hertz':'26',
                 'type':'narrow',
                 'winBidder': '',
-                'winPrice': 0,
+                'winPrice': '',
                 'bidders': [
-                    {'name':'KT', 'price':0},
-                    {'name':'SK', 'price':0},
-                    {'name':'LG', 'price':0}
+                    {'name':'KT', 'price':''},
+                    {'name':'SK', 'price':''},
+                    {'name':'LG', 'price':''}
                 ]
             },
         ]
     },
+    // 'startPrices': {
+    //     'frequency' : [
+    //         {
+    //             'name': 'A',
+    //             'bandWidth':40,
+    //             'hertz':'700',
+    //             'type':'wideBand',
+    //             'winBidder': '',
+    //             'winPrice': 1200,
+    //             'bidders': [
+    //                 {'name':'KT', 'price':1200, 'vs':'win'},
+    //                 {'name':'SK', 'price':1200, 'vs':'win'},
+    //                 {'name':'LG', 'price':1200, 'vs':'win'}
+    //             ]
+    //         },
+    //         {
+    //             'name': 'B',
+    //             'bandWidth':20,
+    //             'hertz':'18',
+    //             'type':'narrow',
+    //             'winBidder': '',
+    //             'winPrice': 2300,
+    //             'bidders': [
+    //                 {'name':'KT', 'price':2300, 'vs':'win'},
+    //                 {'name':'SK', 'price':2300, 'vs':'win'},
+    //                 {'name':'LG', 'price':2300, 'vs':'win'}
+    //             ]
+    //         },
+    //         {
+    //             'name': 'C',
+    //             'bandWidth':20,
+    //             'hertz':'21',
+    //             'type':'wideBand',
+    //             'winBidder': '',
+    //             'winPrice': 3150,
+    //             'bidders': [
+    //                 {'name':'KT', 'price':3150, 'vs':'win'},
+    //                 {'name':'SK', 'price':3150, 'vs':'win'},
+    //                 {'name':'LG', 'price':3150, 'vs':'win'}
+    //             ]
+    //         },
+    //         {
+    //             'name': 'D',
+    //             'bandWidth':40,
+    //             'hertz':'26',
+    //             'type':'wideBand',
+    //             'winBidder': '',
+    //             'winPrice': 3816,
+    //             'bidders': [
+    //                 {'name':'KT', 'price':3816, 'vs':'win'},
+    //                 {'name':'SK', 'price':3816, 'vs':'win'},
+    //                 {'name':'LG', 'price':3816, 'vs':'win'}
+    //             ]
+    //         },
+    //         {
+    //             'name': 'E',
+    //             'bandWidth':20,
+    //             'hertz':'26',
+    //             'type':'narrow',
+    //             'winBidder': '',
+    //             'winPrice': 3277,
+    //             'bidders': [
+    //                 {'name':'KT', 'price':3277, 'vs':'win'},
+    //                 {'name':'SK', 'price':3277, 'vs':'win'},
+    //                 {'name':'LG', 'price':3277, 'vs':'win'}
+    //             ]
+    //         },
+    //     ]
+    // },
+
     // 'frequency' : [
     //     {
     //         'name': 'A',
@@ -232,73 +233,73 @@ define({
     //     },
     // ],
 
-    'frequency' : [
-        {
-            'name': 'A',
-            'bandWidth':40,
-            'hertz':'700',
-            'type':'wideBand',
-            'winBidder': '',
-            'winPrice': 0,
-            'bidders': [
-                {'name':'KT', 'price':0},
-                {'name':'SK', 'price':0},
-                {'name':'LG', 'price':0}
-            ],
-        },
-        {
-            'name': 'B',
-            'bandWidth':20,
-            'hertz':'18',
-            'type':'narrow',
-            'winBidder': '',
-            'winPrice': 0,
-            'bidders': [
-                {'name':'KT', 'price':0},
-                {'name':'SK', 'price':0},
-                {'name':'LG', 'price':0}
-            ],
-        },
-        {
-            'name': 'C',
-            'bandWidth':20,
-            'hertz':'21',
-            'type':'wideBand',
-            'winBidder': '',
-            'winPrice': 0,
-            'bidders': [
-                {'name':'KT', 'price':0},
-                {'name':'SK', 'price':0},
-                {'name':'LG', 'price':0}
-            ],
-        },
-        {
-            'name': 'D',
-            'bandWidth':40,
-            'hertz':'26',
-            'type':'wideBand',
-            'winBidder': '',
-            'winPrice': 0,
-            'bidders': [
-                {'name':'KT', 'price':0},
-                {'name':'SK', 'price':0},
-                {'name':'LG', 'price':0}
-            ],
-        },
-        {
-            'name': 'E',
-            'bandWidth':20,
-            'hertz':'26',
-            'type':'narrow',
-            'winBidder': '',
-            'winPrice': 0,
-            'bidders': [
-                {'name':'KT', 'price':0},
-                {'name':'SK', 'price':0},
-                {'name':'LG', 'price':0}
-            ],
-        },
-    ],
-    color: "black",
-    size: "unisize"
+    // 'frequency' : [
+    //     {
+    //         'name': 'A',
+    //         'bandWidth':40,
+    //         'hertz':'700',
+    //         'type':'wideBand',
+    //         'winBidder': '',
+    //         'winPrice': 0,
+    //         'bidders': [
+    //             {'name':'KT', 'price':0},
+    //             {'name':'SK', 'price':0},
+    //             {'name':'LG', 'price':0}
+    //         ],
+    //     },
+    //     {
+    //         'name': 'B',
+    //         'bandWidth':20,
+    //         'hertz':'18',
+    //         'type':'narrow',
+    //         'winBidder': '',
+    //         'winPrice': 0,
+    //         'bidders': [
+    //             {'name':'KT', 'price':0},
+    //             {'name':'SK', 'price':0},
+    //             {'name':'LG', 'price':0}
+    //         ],
+    //     },
+    //     {
+    //         'name': 'C',
+    //         'bandWidth':20,
+    //         'hertz':'21',
+    //         'type':'wideBand',
+    //         'winBidder': '',
+    //         'winPrice': 0,
+    //         'bidders': [
+    //             {'name':'KT', 'price':0},
+    //             {'name':'SK', 'price':0},
+    //             {'name':'LG', 'price':0}
+    //         ],
+    //     },
+    //     {
+    //         'name': 'D',
+    //         'bandWidth':40,
+    //         'hertz':'26',
+    //         'type':'wideBand',
+    //         'winBidder': '',
+    //         'winPrice': 0,
+    //         'bidders': [
+    //             {'name':'KT', 'price':0},
+    //             {'name':'SK', 'price':0},
+    //             {'name':'LG', 'price':0}
+    //         ],
+    //     },
+    //     {
+    //         'name': 'E',
+    //         'bandWidth':20,
+    //         'hertz':'26',
+    //         'type':'narrow',
+    //         'winBidder': '',
+    //         'winPrice': 0,
+    //         'bidders': [
+    //             {'name':'KT', 'price':0},
+    //             {'name':'SK', 'price':0},
+    //             {'name':'LG', 'price':0}
+    //         ],
+    //     },
+    // ],
+    // color: "black",
+    // size: "unisize"
 });
