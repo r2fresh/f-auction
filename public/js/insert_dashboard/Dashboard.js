19d0d307be0f3a2c01a69732e242e25a51aec814
r2fresh <r2fresh78@gmail.com>
2016-04-25 14:00:36 +0900

    insert_dashboard 누적증분율 계산적용

--- public/js/insert_dashboard/Dashboard.js
+++ public/js/insert_dashboard/Dashboard.js
@@ -171,33 +171,115 @@ define([
         getRoundRateIncreaseList:function(data){
 
             var roundList = JSON.parse(JSON.stringify(data));
-            var startPriceList   = JSON.parse(JSON.stringify(AuctionData.startPriceList));
+            var tempRoundList = null, tempBidderList = null, tempMaxList = null;
 
             for(var i=0; i<roundList.length; ++i){
-                var roundData = roundList[i];
-                var frequencyList = roundData.frequency;
-                for(var j=0; j<frequencyList.length; ++j){
-                    var bidderList = frequencyList[j].bidders;
 
-                    for(var k=0; k<bidderList.length; ++k){
+                tempRoundList = [];
+                for(var m=0; m<i+1; ++m){
+                    tempRoundList.push( JSON.parse(JSON.stringify(roundList[m])) );
+                }
 
-                        var bidder = bidderList[k];
+                tempBidderList = this.getRoundHistoryPrice(tempRoundList);
+                tempMaxList = this.getRoundMaxPrice(tempBidderList);
 
-                        var price = parseInt(bidder.price,10);
-
-                        if(bidder.price == ''){
-                            bidder.rateIncrease = 0;
-                        } else {
-                            var startPrice = parseInt(startPriceList[j].price,10);
-                            var percent = ( (price - startPrice)/startPrice )*100;
-
-                            bidder.rateIncrease = Math.round(percent * 100)/100;
-                        }
+                // 각 라운드 별 입찰자에 해당하는 가격리스트와 미신청 주파수 설정
+                for(var j=0; j<roundList[i].frequency.length; ++j){
+                    for(var k=0; k<roundList[i].frequency[j].bidders.length; ++k){
+                        roundList[i].frequency[j].bidders[k].rateIncrease = parseFloat(tempMaxList[k].rateIncreaseList[j].rateIncrease)
                     }
+                }
 
+                tempRoundList = null;
+                tempBidderList = null;
+                tempMaxList = null;
+            }
+            //var roundthis.getRoundHistoryPrice(data);
+
+            // var roundList = JSON.parse(JSON.stringify(data));
+            // var startPriceList   = JSON.parse(JSON.stringify(AuctionData.startPriceList));
+            //
+            // for(var i=0; i<roundList.length; ++i){
+            //     var roundData = roundList[i];
+            //     var frequencyList = roundData.frequency;
+            //     for(var j=0; j<frequencyList.length; ++j){
+            //         var bidderList = frequencyList[j].bidders;
+            //
+            //         for(var k=0; k<bidderList.length; ++k){
+            //
+            //             var bidder = bidderList[k];
+            //
+            //             var price = parseInt(bidder.price,10);
+            //
+            //             if(bidder.price == ''){
+            //                 bidder.rateIncrease = 0;
+            //             } else {
+            //                 var startPrice = parseInt(startPriceList[j].price,10);
+            //                 var percent = ( (price - startPrice)/startPrice )*100;
+            //
+            //                 bidder.rateIncrease = Math.round(percent * 100)/100;
+            //             }
+            //         }
+            //
+            //     }
+            // }
+            return roundList;
+        },
+        /**
+        * 각 주파수 가격 히스토리를 구하는 함수
+        */
+        getRoundHistoryPrice:function(data){
+            var roundList        = JSON.parse(JSON.stringify(data));
+            var rateIncreaseList = JSON.parse(JSON.stringify(AuctionData.defaultPriceList));
+            var bidderList       = [{'name':'KT'},{'name':'SK'},{'name':'LG'}]
+
+            // 기본 변수 설정
+            _.each(rateIncreaseList,function(item){
+                item.historyPriceList = [];
+            })
+
+            _.each(bidderList, function(item){
+                item.rateIncreaseList = JSON.parse(JSON.stringify(rateIncreaseList));
+            })
+
+            // 각 라운드 별 입찰자에 해당하는 가격리스트와 미신청 주파수 설정
+            for(var i=0; i<roundList.length; ++i){
+                for(var j=0; j<roundList[i].frequency.length; ++j){
+                    for(var k=0; k<roundList[i].frequency[j].bidders.length; ++k){
+                        var bidderPrice = roundList[i].frequency[j].bidders[k].price;
+                        var price = (bidderPrice == '') ? 0 : bidderPrice;
+                        bidderList[k].rateIncreaseList[j].historyPriceList.push(price);
+                    }
                 }
             }
-            return roundList;
+
+            return bidderList;
+        },
+        /**
+        * 각 주파수 가격 히스토리에서 최대가를 추출하고,
+        * 그 가격과 시작가를 비교해서 누적 증분율을 구하는 함수
+        */
+        getRoundMaxPrice:function(data){
+
+            var bidderList        = JSON.parse(JSON.stringify(data));
+            var startPriceList   = JSON.parse(JSON.stringify(AuctionData.startPriceList));
+
+            _.each(bidderList,function(bidder){
+                _.each(bidder.rateIncreaseList,function(item,index){
+                    item.maxPrice = _.max(item.historyPriceList);
+                    if(item.maxPrice == 0){
+                        item.rateIncrease = 0;
+                    } else {
+                        var startPrice = parseInt(startPriceList[index].price,10);
+                        var percent = ( (item.maxPrice - startPrice)/startPrice )*100
+
+                        // 소수점 2자리수 반올림
+                        item.rateIncrease = Math.round(percent * 100)/100;
+                    }
+                });
+            })
+
+            return bidderList;
         },
         /**
          * 라운드별 주파수 정보를 저장
